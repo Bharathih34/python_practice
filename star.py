d37cd40ba36af869c85e330bfda1e4e664d6874c
Bharathi H <112745602+Bharathih34@users.noreply.github.com>
2023-02-05 21:58:04 +0530

    printing a right-angled triangle of asterisks

--- star.py
+++ star.py
@@ -3,3 +3,10 @@ for i in range(1,n+1):
     print("*" *(2*(n-i)+1))
     print(" " * (i-1), end="")
     
+    
+printing a right-angled triangle of asterisks with a base of 5 asterisks. 
+    
+The output will look like this:
+ *****
+ ***
+ *
